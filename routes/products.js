79aced719e0aa15625225c590cad38714f7debb5
Jean Oliveira <jcwilbertt@gmail.com>
2026-03-04 17:14:47 +0000

    Update products.js

--- routes/products.js
+++ routes/products.js
@@ -1,2 +1,7 @@
 import express from 'express'
 import { getProducts, getGenres } from '../controllers/productsControllers.js'
+
+export const productsRouter = express.Router()
+
+productsRouter.get('/genres', getGenres)
+productsRouter.get('/', getProducts)
