3116a174a69dccc336e6ed24434bf7709287e222
Jean Oliveira <jcwilbertt@gmail.com>
2026-03-06 14:50:10 +0000

    Update getData.js

---
+++ getData.js
@@ -0,0 +1,18 @@
+import sqlite3 from 'sqlite3'
+import { open } from 'sqlite'
+import path from 'node:path'
+
+async function getData() {
+    const db = await open({
+        filename: path.join('database.db'),
+        driver: sqlite3.Database
+    })
+
+    try {
+
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+getData()
