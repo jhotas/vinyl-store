8bc0531cd61d88283e5ca64c81740a22b1f08d28
Jean Oliveira <jcwilbertt@gmail.com>
2026-03-09 15:06:37 +0000

    Update productsControllers.js

--- controllers/productsControllers.js
+++ controllers/productsControllers.js
@@ -1,5 +1,13 @@
-export async function getGenres() {
-    console.log('genres')
+import { getDBConnection } from "../db/db.js"
+
+export async function getGenres(req, res) {
+    await getDBConnection()
+
+    try {
+        
+    } catch (error) {
+        res.status(500).json({error: 'Failed to fetch genres', details: error.message})
+    }
 }
 
 export async function getProducts() {
