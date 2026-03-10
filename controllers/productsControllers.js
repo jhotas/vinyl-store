e039a14ff3329b534883208a7c119bc469006e8c
Jean Oliveira <jcwilbertt@gmail.com>
2026-03-10 10:33:09 +0000

    Update productsControllers.js

--- controllers/productsControllers.js
+++ controllers/productsControllers.js
@@ -1,10 +1,12 @@
 import { getDBConnection } from "../db/db.js"
 
 export async function getGenres(req, res) {
-    await getDBConnection()
-
     try {
-        
+        const db = await getDBConnection()
+
+        const genreRows = await db.all('SELECT DISTINCT genre FROM products')
+        const genres = genreRows.map(row => row.genre)
+        res.json(genres)
     } catch (error) {
         res.status(500).json({error: 'Failed to fetch genres', details: error.message})
     }
